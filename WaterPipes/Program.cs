ebf97b48b75aa55c03afe55b5aa99de5d422fb6a
melnyk-a <melnyk.an.il@gmail.com>
2018-04-19 18:15:06 +0300

    Add project files.

---
+++ WaterPipes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterPipes
+{
+    internal sealed class Program
+    {
+        static void Main(string[] arguments)
+        {
+        }
+    }
+}
